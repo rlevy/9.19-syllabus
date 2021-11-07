5f64aead36123d12f600b567cc3d231d0d9a5a91
Roger Levy <rlevy@users.noreply.github.com>
2021-11-07 17:00:21 -0500

    fixed lines

--- org_to_markdown_table.py
+++ org_to_markdown_table.py
@@ -5,6 +5,7 @@ outfile = open("syllabus.markdown","w")
 
 p = re.compile(r"^(.*?)\[\[([^\]]*)\]\[([^\]]*)\]\](.*)$")
 for line in infile.readlines():
+    line = line.rstrip()
     if re.match("^[0-9|<> ]*$",line) and re.match(".*\|.*",line):
         continue
     stillSearchingLine = True
@@ -14,5 +15,5 @@ for line in infile.readlines():
             stillSearchingLine = False
             continue
         line = m.group(1) + "[" + m.group(3) + "](" + m.group(2) + ")" + m.group(4)
-    outfile.write(line)
+    outfile.write(line + "\n")
         
